bf14d5d935b1210d75f35a8609c9f8b761af5b1b
emicon-web <emiconweb@emicon.co.in>
2021-04-27 17:04:09 +0530

    first commit

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI , { useNewUrlParser: true , useUnifiedTopology: true ,useCreateIndex: true } ,
+mongoose.connect("mongodb+srv://engagement:Mongodb@123@cluster0.r17es.mongodb.net/bet?retryWrites=true&w=majority" , { useNewUrlParser: true , useUnifiedTopology: true ,useCreateIndex: true } ,
     (err) => {
     if(!err) { console.log('Mongo DB connected succeeded'); }
     else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined , 2)); }
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ require('./startup/prod') (app);
 require('./models/db');
 
 require('./startup/routes') (app);
-
-app.listen( process.env.PORT,err=>{
+let port = process.env.PORT || 5000;
+app.listen( port,err=>{
     if(err){console.log(err);}
     console.log(`Server Started On Port : ${process.env.PORT}`);
 })
