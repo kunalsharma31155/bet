f2cbe33096a300dd8e5fe9fa014e298953b5ec9e
emicon-web <emiconweb@emicon.co.in>
2021-04-27 20:05:27 +0530

    fd

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'index.html'))
 })
 var PORT = process.env.PORT || 5000;
-app.listen( process.env.PORT || 3000, process.env.HOST || '::',err=>{
+app.listen( process.env.PORT || 3000, process.env.HOST || '0.0.0.0'|| '::',err=>{
     if(err){console.log(err);}
     console.log(`Server Started On Portt : ${process.env.PORT}`);
 }) 
